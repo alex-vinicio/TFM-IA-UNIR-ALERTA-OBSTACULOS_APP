7dab877af0e6b16ac9a4c79ac866325e276a8672
alex-vinicio <38170743+alex-vinicio@users.noreply.github.com>
2024-06-27 03:15:40 +0100

    Create listUserLogsAnalitic.py

---
+++ listUserLogsAnalitic.py
@@ -0,0 +1,33 @@
+import boto3
+import json
+
+dynamodb = boto3.resource('dynamodb')
+
+
+def respond(err, res=None):
+    return {
+        'statusCode': '400' if err else '200',
+        'body': err.message if err else json.dumps(res),
+        'headers': {
+            'Content-Type': 'application/json',
+        },
+    }
+
+
+def lambda_handler(event, context):
+    
+    # Nombre de la tabla
+    table_name = 'DB_USER_LOGS_APP'
+    
+    # Obtener la tabla
+    table = dynamodb.Table(table_name)
+    
+    try:
+        response = table.scan()
+        
+        # Procesar y devolver la respuesta
+        items = response['Items']
+        return respond(None, items)
+    
+    except Exception as e:
+        return respond(ValueError('Unsupported method "{}"'.format(operation)))
